a6f14d6f5242b1006fa495ba9f86a9d2a690b30d
sardordev20 <uroqovsardor510@gmail.com>
2023-04-17 11:18:23 +0500

    fanction componentdan class componentga o'tkazish

--- src/components/movies-add-form/movies-add-form.js
+++ src/components/movies-add-form/movies-add-form.js
@@ -1,16 +1,19 @@
+import { Component } from 'react';
 import './movies-add-form.css';
 
-const MoviesAddForm = () => {
-  return (
-    <div className='movies-add-form'>
-      <h3>Yangi kino qo'shish</h3>
-      <form className='add-form d-flex'>
-        <input type="text" className='form-control new-post-label' placeholder='Qanday kino?' />
-        <input type="number" className='form-control new-post-label' placeholder="Nechi marotaba ko'rilgan?" />
-        <button type='submit' className='btn btn-outline-dark'>Qo'shish</button>
-      </form>
-    </div>
-  )
+class MoviesAddForm extends Component {
+  render() {
+    return (
+      <div className='movies-add-form'>
+        <h3>Yangi kino qo'shish</h3>
+        <form className='add-form d-flex'>
+          <input type="text" className='form-control new-post-label' placeholder='Qanday kino?' />
+          <input type="number" className='form-control new-post-label' placeholder="Nechi marotaba ko'rilgan?" />
+          <button type='submit' className='btn btn-outline-dark'>Qo'shish</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default MoviesAddForm
+export default MoviesAddForm;
