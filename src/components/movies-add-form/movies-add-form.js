fae52ebe0e5c06c0d097a7cabfc3806a02054b42
sardordev20 <uroqovsardor510@gmail.com>
2023-04-17 11:35:52 +0500

    unikal inputlar bilan ishlash funksiyasi

--- src/components/movies-add-form/movies-add-form.js
+++ src/components/movies-add-form/movies-add-form.js
@@ -1,25 +1,46 @@
-import { Component } from 'react';
-import './movies-add-form.css';
+import { Component } from "react";
+import "./movies-add-form.css";
 
 class MoviesAddForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      views: '',
+      name: "",
+      views: "",
     };
   }
+
+  changeHandlerInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
     return (
-      <div className='movies-add-form'>
+      <div className="movies-add-form">
         <h3>Yangi kino qo'shish</h3>
-        <form className='add-form d-flex'>
-          <input type="text" className='form-control new-post-label' placeholder='Qanday kino?' />
-          <input type="number" className='form-control new-post-label' placeholder="Nechi marotaba ko'rilgan?" />
-          <button type='submit' className='btn btn-outline-dark'>Qo'shish</button>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Qanday kino?"
+            onChange={this.changeHandlerInput}
+            name="name"
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="Nechi marotaba ko'rilgan?"
+            onChange={this.changeHandlerInput}
+            name="views"
+          />
+          <button type="submit" className="btn btn-outline-dark">
+            Qo'shish
+          </button>
         </form>
       </div>
-    )
+    );
   }
 }
 
