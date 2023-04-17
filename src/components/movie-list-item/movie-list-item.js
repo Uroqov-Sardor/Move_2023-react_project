c1c0f30dea4027ff115d1f1c1314a84a7a5caf4a
sardordev20 <uroqovsardor510@gmail.com>
2023-04-17 11:15:19 +0500

    movie-list-item commentaria delete

--- src/components/movie-list-item/movie-list-item.js
+++ src/components/movie-list-item/movie-list-item.js
@@ -43,22 +43,4 @@ class MovieListItem extends Component {
   }
 }
 
-// const MovieListItem = ({  name, viewers, favourite }) => {
-//   return (
-//     <li className={`list-group-item d-flex justfiy-content-between ${favourite && "favourite"}`}>
-//       <span className='list-group-item-label'>{name}</span>
-//       <input type="number" className='list-group-item-input' defaultValue={viewers} />
-//       <div className='d-flex justfiy-content-center align-items-center'>
-//         <button type='button' className='btn-cookie btn-sm'>
-//           <i className='fas fa-cookie'></i>
-//         </button>
-//         <button type='button' className='btn-trash btn-sm'>
-//           <i className='fas fa-trash'></i>
-//         </button>
-//         <i className='fas fa-star'></i>
-//       </div>
-//     </li>
-//   )
-// }
-
 export default MovieListItem
