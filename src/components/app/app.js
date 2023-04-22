1563edf9ae6eb00388fe4174abb831d082ef7203
sardordev20 <uroqovsardor510@gmail.com>
2023-04-22 20:48:12 +0500

    Loyhaga xodisalar qo'shish

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import "./app.css";
 
 import { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -12,9 +13,27 @@ class App extends Component {
     super(props);
     this.state = {
       data: [
-        { name: "Shelbeylar oilasi", viewers: 850, favourite: false, id: 1 },
-        { name: "Aka-uka Creylar", viewers: 1050, favourite: true, id: 2 },
-        { name: "Panjara ortida", viewers: 1050, favourite: false, id: 3 },
+        {
+          name: "Shelbeylar oilasi",
+          viewers: 850,
+          favourite: false,
+          like: false,
+          id: 1,
+        },
+        {
+          name: "Aka-uka Creylar",
+          viewers: 1050,
+          favourite: false,
+          like: false,
+          id: 2,
+        },
+        {
+          name: "Panjara ortida",
+          viewers: 1050,
+          favourite: false,
+          like: false,
+          id: 3,
+        },
       ],
     };
   }
@@ -24,22 +43,46 @@ class App extends Component {
   };
 
   addForm = (item) => {
+    const newItem = {
+      name: item.name,
+      viewers: item.viewers,
+      id: uuidv4(),
+      favourite: false,
+      like: false,
+    };
     this.setState(({ data }) => ({
-      data: [...data, item],
+      data: [...data, newItem],
+    }));
+  };
+
+  onToggleProp = (id, prop) => {
+    this.setState(({ data }) => ({
+      data: data.map((item) => {
+        if (item.id === id) {
+          return { ...item, [prop]: !item[prop] };
+        }
+        return item;
+      }),
     }));
   };
 
   render() {
     const { data } = this.state;
+    const allMoviesCount = data.length;
+    const allMovieFavourite = data.filter(c => c.favourite).length
     return (
       <div className="app font-monospace">
         <div className="content">
-          <AppInfo />
+          <AppInfo allMoviesCount={allMoviesCount} allMovieFavourite={allMovieFavourite} />
           <div className="search-panel">
             <SearchPanel />
             <AppFilter />
           </div>
-          <MovieList data={data} onDelete={this.onDelete} />
+          <MovieList
+            data={data}
+            onDelete={this.onDelete}
+            onToggleProp={this.onToggleProp}
+          />
           <MoviesAddForm addForm={this.addForm} />
         </div>
       </div>
--- src/components/movie-list-item/movie-list-item.js
+++ src/components/movie-list-item/movie-list-item.js
@@ -1,46 +1,22 @@
-import { Component } from 'react';
 import './movie-list-item.css';
 
-class MovieListItem extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      favourite: false,
-      like: false,
-    };
-  };
-
-  onFavourite = () => {
-    this.setState(({ favourite }) => ({
-      favourite: !favourite,
-    }));
-  };
-
-  onLike = () => {
-    this.setState(({ like }) => ({
-      like: !like,
-    }));
-  };
-
-  render() {
-    const {  name, viewers, onDelete } = this.props;
-    const { favourite, like } = this.state;
-    return (
-      <li className={`list-group-item d-flex justfiy-content-between ${favourite && "favourite"} ${like && "like"}`}>
-        <span onClick={this.onLike} className='list-group-item-label'>{name}</span>
-        <input type="number" className='list-group-item-input' defaultValue={viewers} />
-        <div className='d-flex justfiy-content-center align-items-center'>
-          <button type='button' className='btn-cookie btn-sm' onClick={this.onFavourite}>
-            <i className='fas fa-cookie'></i>
-          </button>
-          <button type='button' className='btn-trash btn-sm' onClick={onDelete}>
-            <i className='fas fa-trash'></i>
-          </button>
-          <i className='fas fa-star'></i>
-        </div>
-      </li>
-    )
-  }
+const MovieListItem = (props) => {
+  const {  name, viewers, onDelete, onToggleProp, favourite, like } = props;
+  return (
+    <li className={`list-group-item d-flex justfiy-content-between ${favourite && "favourite"} ${like && "like"}`}>
+      <span onClick={onToggleProp} data-toggle="like" className='list-group-item-label'>{name}</span>
+      <input type="number" className='list-group-item-input' defaultValue={viewers} />
+      <div className='d-flex justfiy-content-center align-items-center'>
+        <button type='button' className='btn-cookie btn-sm' onClick={onToggleProp} data-toggle="favourite" >
+          <i className='fas fa-cookie'></i>
+        </button>
+        <button type='button' className='btn-trash btn-sm' onClick={onDelete}>
+          <i className='fas fa-trash'></i>
+        </button>
+        <i className='fas fa-star'></i>
+      </div>
+    </li>
+  )
 }
 
 export default MovieListItem
